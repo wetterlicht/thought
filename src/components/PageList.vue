a7fea2c98412b7ae726cdea4b127f99285133bfd
Matthias Nötzli <matthias.noetzli@proton.me>
2025-05-18 10:54:29 +0200

    Add ability to toggle nested children

--- src/components/PageList.vue
+++ src/components/PageList.vue
@@ -1,34 +1,14 @@
 <template>
     <ul class="w-full flex flex-col gap-y-px">
-        <li v-for="page in pages" :key="page.id" class="w-full">
-
-            <div class="group relative">
-                <RouterLink :to="`/workspaces/${workspaceId}/pages/${page.id}`" class="cursor-default text-stone-600 hover:text-black
-                hover:bg-stone-200 block rounded-lg leading-6 px-2 py-1.5">
-                    <div class="flex items-center gap-1" :style="{ 'margin-left': `${depth * 0.5}rem` }">
-                        <div v-if="page.icon">{{ page.icon }}</div>
-                        <DocumentTextIcon class="size-5 shrink-0" v-else></DocumentTextIcon>
-                        <div class="truncate flex-grow">{{ page.title }}</div>
-                    </div>
-                </RouterLink>
-                <button @click.stop="createPageInPage(page.id)"
-                    class="absolute right-1 top-1 text-stone-600 
-                    opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 hover:text-black hover:bg-stone-300 rounded-md p-1">
-                    <DocumentPlusIcon class="size-5"></DocumentPlusIcon>
-                </button>
-            </div>
-            <PageList v-if="page.children" :workspaceId="workspaceId" :pages="page.children" :depth="depth + 1">
-            </PageList>
-        </li>
+        <PageListElement v-for="page in pages" :key="page.id" :workspaceId="workspaceId" :page="page" :depth="depth"
+            @createPageInPage="$emit('createPageInPage', $event)">
+        </PageListElement>
     </ul>
 </template>
 
 <script setup lang="ts">
-import { DocumentTextIcon, DocumentPlusIcon } from "@heroicons/vue/24/outline";
 import type { PageDocumentWithId } from '@/types';
-import { useRepo } from '@/composables/useRepo';
-
-const { createPageInPage } = useRepo();
+import PageListElement from './PageListElement.vue';
 
 const props = defineProps({
     workspaceId: {
@@ -46,9 +26,4 @@ const props = defineProps({
 })
 </script>
 
-<style scoped>
-.router-link-active {
-    color: var(--color-black);
-    background-color: var(--color-stone-200);
-}
-</style>
+<style scoped></style>
---
+++ src/components/PageListElement.vue
@@ -0,0 +1,63 @@
+<template>
+    <li class="w-full">
+        <div class="group relative">
+            <RouterLink :to="`/workspaces/${workspaceId}/pages/${page.id}`" class="cursor-default text-stone-600 group-hover:text-black
+                group-hover:bg-stone-200 block rounded-lg leading-6 px-2 py-1.5">
+                <div class="flex items-center gap-1" :style="{ 'margin-left': `${depth * 0.75}rem` }">
+                    <div v-if="page.icon">{{ page.icon }}</div>
+                    <DocumentTextIcon class="size-5 shrink-0" v-else></DocumentTextIcon>
+                    <div class="truncate flex-grow">{{ page.title }}</div>
+                </div>
+            </RouterLink>
+            <button @click.stop="createPageInPage(page.id)"
+                class="absolute right-1 top-1 text-stone-600 
+                    opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 hover:text-black hover:bg-stone-300 rounded-md p-1">
+                <DocumentPlusIcon class="size-5"></DocumentPlusIcon>
+            </button>
+            <button v-if="page.children.length" @click="showChildren = !showChildren"
+                class="absolute right-8 top-1 h-7 w-7 flex items-center justify-center text-stone-600 
+                    opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 hover:text-black hover:bg-stone-300 rounded-md p-1">
+                <ChevronRightIcon class="size-4" :class="{
+                    'rotate-90': showChildren
+                }"></ChevronRightIcon>
+            </button>
+        </div>
+        <PageList class="my-px" v-if="page.children && showChildren" :workspaceId="workspaceId" :pages="page.children"
+            :depth="depth + 1">
+        </PageList>
+    </li>
+</template>
+
+<script setup lang="ts">
+import { DocumentTextIcon, DocumentPlusIcon, ChevronRightIcon } from "@heroicons/vue/24/outline";
+import { useRepo } from '@/composables/useRepo';
+import { ref, type PropType } from "vue";
+import PageList from './PageList.vue';
+import type { PageDocumentWithId } from "@/types";
+const { createPageInPage } = useRepo();
+
+const props = defineProps({
+    workspaceId: {
+        type: String,
+        required: true
+    },
+    page: {
+        type: Object as PropType<PageDocumentWithId>,
+        required: true
+    },
+    depth: {
+        type: Number,
+        default: 0
+    }
+})
+
+const showChildren = ref(false);
+
+</script>
+
+<style scoped>
+.router-link-active {
+    color: var(--color-black);
+    background-color: var(--color-stone-200);
+}
+</style>
